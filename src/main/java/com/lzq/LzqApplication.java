4c9046d029da027cbee0fa9ddd92a8fbb48a1efc
jodden <619353515@qq.com>
2020-03-20 13:51:53 +0800

    first

---
+++ src/main/java/com/lzq/LzqApplication.java
@@ -0,0 +1,13 @@
+package com.lzq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LzqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LzqApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lzq/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.lzq.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index(){
+        return "aaa";
+    }
+}
